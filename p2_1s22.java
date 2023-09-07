fdfffd325fc2850bc148dac6c1ac5f4cea58350f
LuisilloSavagge <144190931+LuisilloSavagge@users.noreply.github.com>
2023-09-06 19:06:52 -0600

    Add files via upload

---
+++ p2_1s22.java
@@ -0,0 +1,7 @@
+Class P2_1s22{public static void main(String[] args) {
+	System.out.println("Mendoza Peña Luis Fernando");
+		System.out.println("20");
+			System.out.println("Luisillosavagge@gmail.com");
+	         System.out.println("Pintar Dibujar");
+              System.out.println("Negro");
+}
